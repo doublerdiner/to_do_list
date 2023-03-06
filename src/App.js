f3e0add6ea922764894250b84f0d20e34018c8e7
Chris Barclay <chris.barclay1@gmail.com>
2023-03-06 17:27:23 +0000

    MVP Completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
-  return (
 
+  const [toDoArray, setToDoArray] = useState([
+    {name: "Buy Shopping", priority: true},
+    {name: "Clean Bathroom", priority: false},
+    {name: "Car's MOT", priority: true},
+  ]);
+  const [newToDoName, setNewToDoName] = useState("");
+
+  function handleInputChange(event){
+    setNewToDoName(event.target.value);
+  };
+
+  function handleValueChange(event){
+    console.log(event.target.value);
+  };
+
+
+
+  function saveNewToDo(event){
+    event.preventDefault();
+    const newToDo = {
+      name: newToDoName,
+      priority: false,
+    };
+    const newToDoArray = [...toDoArray, newToDo];
+    setToDoArray(newToDoArray);
+    setNewToDoName("");
+  };
+
+
+
+
+
+
+  const toDoNodes = toDoArray.map((toDo, index)=>{
+    return <div key={index}>
+    {toDo.priority ? <li className="high"><span>{toDo.name}</span></li> : <li className="low"><span>{toDo.name}</span></li> }
+    </div>});
+
+  return (
+    <div id="app">
+      <h1>ToDo's</h1>
+      <form onSubmit={saveNewToDo}>
+        <input className="text-box" type="text" value={newToDoName} onChange={handleInputChange}/>
+        High<input type="radio" value="true" name="high/low" onChange={handleValueChange}/>
+        Low<input type="radio" value="false" name="high/low" onChange={handleValueChange}/>
+        <input className="button" type="submit" value="Save Item"/>      
+      </form>
+      <ul>
+        {toDoNodes}
+      </ul>       
+    </div>
   );
 }
 
