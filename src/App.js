24c964339e390d11f969e6421b9c968d7e243e1b
Chris Barclay <chris.barclay1@gmail.com>
2023-03-06 21:06:59 +0000

    Final Commit. Extensions completed. Delete All button added.

--- src/App.js
+++ src/App.js
@@ -4,53 +4,57 @@ import React, {useState} from 'react';
 function App() {
 
   const [toDoArray, setToDoArray] = useState([
-    {name: "Buy Shopping", priority: true},
-    {name: "Clean Bathroom", priority: false},
-    {name: "Car's MOT", priority: true},
+    {name: "Buy Shopping", priority: "high"},
+    {name: "Clean Bathroom", priority: "low"},
+    {name: "Car's MOT", priority: "high"},
   ]);
   const [newToDoName, setNewToDoName] = useState("");
+  const [newHighLowValue, setNewHighLowValue] = useState("low");
 
   function handleInputChange(event){
     setNewToDoName(event.target.value);
   };
 
   function handleValueChange(event){
-    console.log(event.target.value);
+    console.log(toDoArray);
+    setNewHighLowValue(event.target.value);
   };
-
-
 
   function saveNewToDo(event){
     event.preventDefault();
+    console.log(newHighLowValue);
     const newToDo = {
       name: newToDoName,
-      priority: false,
+      priority: newHighLowValue,
     };
     const newToDoArray = [...toDoArray, newToDo];
     setToDoArray(newToDoArray);
     setNewToDoName("");
   };
 
-
-
-
-
+  function deleteAll(){
+    const list = document.querySelector('#test');
+    list.innerHTML = '';
+  };
 
   const toDoNodes = toDoArray.map((toDo, index)=>{
     return <div key={index}>
-    {toDo.priority ? <li className="high"><span>{toDo.name}</span></li> : <li className="low"><span>{toDo.name}</span></li> }
+    {toDo.priority ==="high" ? <li className="high"><span>{toDo.name}</span></li> : <li className="low"><span>{toDo.name}</span></li> }
     </div>});
 
   return (
     <div id="app">
       <h1>ToDo's</h1>
-      <form onSubmit={saveNewToDo}>
-        <input className="text-box" type="text" value={newToDoName} onChange={handleInputChange}/>
-        High<input type="radio" value="true" name="high/low" onChange={handleValueChange}/>
-        Low<input type="radio" value="false" name="high/low" onChange={handleValueChange}/>
-        <input className="button" type="submit" value="Save Item"/>      
-      </form>
-      <ul>
+      <nav>
+        <form onSubmit={saveNewToDo}>
+          <input className="text-box" type="text" value={newToDoName} onChange={handleInputChange}/>
+          High<input type="radio" value="high" name="high/low" onChange={handleValueChange}/>
+          Low<input type="radio" value="low" name="high/low" onChange={handleValueChange} defaultChecked/>
+          <input className="button" type="submit" value="Save Item"/>      
+        </form>
+        <button className = "button" type='submit' onClick={deleteAll}>Delete All</button>
+      </nav>
+      <ul id="test">
         {toDoNodes}
       </ul>       
     </div>
